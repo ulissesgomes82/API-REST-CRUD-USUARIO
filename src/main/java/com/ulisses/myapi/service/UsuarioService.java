191b4dedc12640d84547fde2627e63600f77d04f
ulissesgomes82 <ulissesdeveloper82@gmail.com>
2022-08-21 23:40:57 -0300

    Criando os metodos findAll, findById, create, update, delete

---
+++ src/main/java/com/ulisses/myapi/service/UsuarioService.java
@@ -0,0 +1,44 @@
+package com.ulisses.myapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ulisses.myapi.domain.Usuario;
+import com.ulisses.myapi.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> findAll(){
+		return usuarioRepository.findAll();
+	}
+	
+	public Usuario FindById(Long id) {
+		Optional<Usuario> obj = usuarioRepository.findById(id);
+		return obj.orElse(null);
+	}
+	
+	public Usuario create(Usuario usuario) {
+		usuario.setId(null);
+		return usuarioRepository.save(usuario);
+	}
+	
+	public Usuario update(Long id, Usuario usuario) {
+		Usuario obj = FindById(id);
+		obj.setName(usuario.getName());
+		obj.setLogin(usuario.getLogin());
+		obj.setSenha(usuario.getSenha());
+		return usuarioRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		usuarioRepository.deleteById(id);
+	}
+	
+}
